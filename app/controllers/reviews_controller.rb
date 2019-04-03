e72d1cc6f2e360b984d3fcd3093c8f62abb82855
Jeremiah <almostgone47@hotmail.com>
2019-04-03 17:42:37 +0800

    user permissions

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,8 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
   before_action :set_restaurant
+  before_action :authenticate_user!
+  before_action :check_user, only: [:edit, :update, :destroy]
 
   # GET /reviews/1
   # GET /reviews/1.json
@@ -52,7 +54,7 @@ class ReviewsController < ApplicationController
   def destroy
     @review.destroy
     respond_to do |format|
-      format.html { redirect_to reviews_url, notice: 'Review was successfully destroyed.' }
+      format.html { redirect_to restaurant_path(@restaurant), notice: 'Review was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -65,6 +67,12 @@ class ReviewsController < ApplicationController
     
     def set_restaurant
       @restaurant = Restaurant.find(params[:restaurant_id])
+    end
+
+    def check_user
+      unless (@review.user == current_user) || (current_user.admin?)
+        redirect_to root_path, notice: "You can only make changes to your own content"
+      end
     end
       # Never trust parameters from the scary internet, only allow the white list through.
     def review_params
